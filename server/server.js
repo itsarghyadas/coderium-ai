52544df1d4c1ff1d70bf2f7a8ef898ff3c640103
Arghya Das <arghyadasproject@gmail.com>
2023-03-03 22:36:45 +0530

    Made the server file cleaner

--- server/server.js
+++ server/server.js
@@ -2,16 +2,10 @@
 import express from "express";
 const app = express();
 import cors from "cors";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 
 // import the database connection
 import connectDB from "./mongodb/connect.js";
-
-// import the models
-import User from "./mongodb/models/user.model.js";
-
 // import the routes
 import router from "./router/route.js";
 
@@ -46,53 +40,4 @@ app.get("/", (req, res) => {
 });
 
 // api routes
-app.use("/api", router);
-
-// openai api key and model id
-import { Configuration, OpenAIApi } from "openai";
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-let context = "";
-
-app.post("/api/message", async (req, res, next) => {
-  const { message } = req.body;
-  if (!message) {
-    return res.status(400).json({ error: "Message is required" });
-  }
-  try {
-    console.log("Message:", message);
-    const inputPrompt = `${context} ${message}`.trim();
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `
-      I want you to reply to all my questions in markdown format. 
-      Q: ${inputPrompt}?.
-      A: `,
-      max_tokens: 400,
-      temperature: 0.7,
-      top_p: 0.5,
-      presence_penalty: 0.2,
-      frequency_penalty: 0.5,
-    });
-    let generatedText = response.data.choices[0].text.trim();
-    console.log("Generated text:", generatedText);
-    if (!generatedText) {
-      // check if generatedText is empty or null
-      generatedText = "I'm sorry, I don't know the answer to that Question.";
-    }
-    context = `${context} ${generatedText}`.trim();
-
-    res.json({ message: generatedText });
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Something went wrong!" });
-});
+app.use("/api", router); // This is the route to use the router
